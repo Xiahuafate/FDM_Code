ed687f0c6d113cb62232c7ddef197504c72db65e
Xiahuafate <90884686+Xiahuafate@users.noreply.github.com>
2021-10-20 17:28:33 +0800

    code
    any change is over ,这是一段非常完美的代码了。添加了各种反应率的输出，增加了直接求逆和追赶法两种求解矩阵的模式。增加了文件数据的输出。

--- FDM_Code.py
+++ FDM_Code.py
@@ -1,11 +1,13 @@
 import numpy as np
 import os
 import math
-import matplotlib as plot
+import time
+import matplotlib.pyplot as plt
 from xml.dom import minidom
 
-# a function for the main
+
 # a function for the input card read
+# this is a function for the error massage information
 def ErrorFunction(i):
     ErrorMassage = ["Error 0 :the material do not have its ID！\n",\
                     "Error 1 :the material do not have its transport cross ssection! \n",\
@@ -23,9 +25,11 @@ def ErrorFunction(i):
                     "Error 13 :the pin layer do not have its coordinates son-node, and it is necsseary!\n",\
                     "Error 14 :the pin layer do not have its MatIDs son-node, and it is necsseary!\n",\
                     "Error 15 :the Matrix Solution Method is not correct!\n",\
+                    "Error 16 :the input of file name is not correct!,Must be xml!\n",\
                     ]
     print(ErrorMassage[i])
     
+
 class settings:
     def __init__(self,settings_tag):
 
@@ -166,7 +170,7 @@ class pin:
             ErrorFunction(11)
             os._exit(0)
         try:
-            self.numnodes = float(pin_tag.getAttribute("NumNodes"))
+            self.numnodes = int(pin_tag.getAttribute("NumNodes"))
         except:
             ErrorFunction(12)
             os._exit(0)
@@ -192,6 +196,18 @@ class pin:
             except:
                 ErrorFunction(14)
                 os._exit(0)
+        elif self.partitionform == "uniform":
+            coordinates = np.linspace(0,self.width,num = int(self.numnodes))
+            coordinates = coordinates.tolist()
+            coordinates.pop(0)
+            [self.coordinates.append(float(i)) for i in coordinates]
+            matids_tag = pin_tag.getElementsByTagName("MatIDs")[0]
+            matids = matids_tag.firstChild.data
+            matids = matids.strip("\n").split()
+            matids = int(matids[0])
+            for i in range(len(coordinates)):
+                self.matids.append(matids)
+
         
 class lattice:
     def __init__(self,geometries_tag,id):
@@ -223,7 +239,6 @@ class lattice:
         for pin_id in np.unique(self.latticegeo):
             self.pin_list.append(pin(geometries_tag,pin_id))
 
-        
 
 class core:
     # This is a class for the core
@@ -251,6 +266,117 @@ class core:
 
         for lattice_id in np.unique(self.coregeo):
             self.lattice_list.append(lattice(geometries_tag,lattice_id)) 
+
+
+def FigSvae(x,y,title,x_label,y_label,):
+    plt.plot(x, y, color='r',marker='o',linestyle='dashed')
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.savefig(title+".jpg")
+    plt.close()
+
+# a function for the outputing of data
+def ScreenPrint(fileinput):
+    screen_str = ["--------------------------------------------------------------------------------------------------------------------------------\n",\
+                  "--------------------------------------------------------------------------------------------------------------------------------\n",\
+                  "|                                                                                                                              |\n",\
+                  "|                                                                                                                              |\n",\
+                  "|                                                                                                                              |\n",\
+                  "|                                                                                                                              |\n",\
+                  "|                                                                                                                              |\n",\
+                  "|                                                                                                                              |\n",\
+                  "|                                                                                                                              |\n",\
+                  "|                                                                                                                              |\n",\
+                  "|                                                                                                                              |\n",\
+                  "|                                                                                                                              |\n",\
+                  "|                                                                                                                              |\n",\
+                  "|                                                                                                                              |\n",\
+                  "|                                                                                                                              |\n",\
+                  "|                                                                                                                              |\n",\
+                  "--------------------------------------------------------------------------------------------------------------------------------\n",\
+                  "--------------------------------------------------------------------------------------------------------------------------------\n",\
+                  ""]
+    if fileinput[-3:] != "xml":
+        ErrorFunction(16)
+        os._exit(0)
+    fileout = fileinput[:-3] + "out"
+    f = open(fileout,"w+")
+    f.close()
+    command = "copy " + fileout + "+" + fileinput
+    os.system(command)
+    os.system("cls")
+    f = open(fileout,"a+")
+    f.write("\n")
+    for i in range(len(screen_str)):
+        print(screen_str[i])
+        f.write(screen_str[i])
+    now_time = "Now Time Is: " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "\n"
+    print(now_time)
+    f.write(now_time)
+    f.close()
+    return fileout
+
+
+def DataPrint(fileout,setting,k_inf,k_error,nodes,flux,absorption_rate,nufission_rate,leak_rate):
+    f = open(fileout,"a+")
+    #print the k change in the screen
+    str1 = "The initial effective proliferation factor is {:.8f}\n".format(k_inf[0])
+    print("\n"+str1)
+    f.write(str1)
+    str1 = "     Iterations_step                       K_inf                                    K_error\n"
+    print(str1)
+    f.write(str1)
+    for i in range(len(k_error)):
+        str1 = "            {:}                           {:.8f}                                {:.8f}\n".format((i+1),k_inf[i+1],k_error[i])
+        print(str1)
+        f.write(str1)
+    now_time = "Now Time Is: " + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()) + "\n"
+    print(now_time)
+    f.write(now_time)
+    # check the options of output
+    if setting.flux_density == 1:
+        flux = flux.tolist()
+        print("Start printing the distribution of neutron flux density over space\n")
+        str1 = "\n" + "                 nodes                           FluxDensity                     \n"
+        f.write(str1)
+        for i in range(len(nodes)):
+            str1 = "                 {:.4}                          {:.9f}                     \n".format(nodes[i],flux[i][0])
+            f.write(str1)
+        FigSvae(nodes,flux,"Neutron flux density distribution","Nodes","Neutron flux density")
+        print("End of the print\n")
+    if setting.fission_rate == 1:
+        nufission_rate = nufission_rate.tolist()
+        print("Start printing the distribution of Nufission rate over space\n")
+        str1 = "\n" + "                 nodes                           NufissionRate                     \n"
+        f.write(str1)
+        for i in range(len(nodes)):
+            str1 = "                 {:.4}                          {:.9f}                     \n".format(nodes[i],nufission_rate[i][0])
+            f.write(str1)
+        FigSvae(nodes,nufission_rate,"Nufission rate distribution","Nodes","Nufission Rate")
+        print("End of the print\n")
+    if setting.absorption_rate == 1:
+        absorption_rate = absorption_rate.tolist()
+        print("Start printing the distribution of Absorption rate over space\n")
+        str1 = "\n" + "                 nodes                           AbsorptionRate                     \n"
+        f.write(str1)
+        for i in range(len(nodes)):
+            str1 = "                 {:.4}                          {:.9f}                     \n".format(nodes[i],absorption_rate[i][0])
+            f.write(str1)
+        FigSvae(nodes,absorption_rate,"Absorption rate distribution","Nodes","Absorption Rate")
+        print("End of the print\n")
+    if setting.leak_rate == 1:
+        leak_rate = leak_rate.tolist()
+        print("Start printing the distribution of Leak rate over space\n")
+        str1 = "\n" + "                 nodes                           LeakRate                     \n"
+        f.write(str1)
+        for i in range(len(nodes)):
+            str1 = "                 {:.4}                          {:.9f}                     \n".format(nodes[i],leak_rate[i][0])
+            f.write(str1)
+        FigSvae(nodes,leak_rate,"Leak rate distribution","Nodes","Leak Rate")
+        print("End of the print\n")
+    f.close()
+
 
 def InputRead(inputfile):
     file_xml = minidom.parse(inputfile)
@@ -313,7 +439,46 @@ def CreateMartix(setting,material_list,coredata):
         matrix_m[i,i] = (absorptioni * detai)/2.0 + (absorptioni_1 * detai_1)/2 + Di_1/detai_1 + Di/detai
         matrix_m[i,i+1] = -Di_1/detai_1
         matrix_f[i,0] = nufissioni * detai /2.0 + nufissioni_1* detai_1 / 2.0
-    return matrix_m,matrix_f
+    return matrix_m,matrix_f,nodes,mat_nodes
+
+
+# get the flux and calculate other parameter
+def DataCalculate(mode,material_list,setting,mat_nodes,nodes,flux):
+    #calculate the absorption
+    if mode=="absorption":
+        absorption_xs = np.matrix(np.zeros((int(len(mat_nodes)),1)))
+        absorption_rate = np.matrix(np.zeros((int(flux.shape[0]),1)))
+        for i in range(len(mat_nodes)):
+            absorption_xs[i,0] = material_list[mat_nodes[i]].absorption[0]
+        absorption_rate[0,0] = flux[0,0] * absorption_xs[0,0]
+        for i in range(len(mat_nodes)):
+            absorption_rate[i+1,0] = flux[i+1,0] * absorption_xs[i,0]
+        returndata = absorption_rate
+    elif mode == "nufission":
+        nufission_xs = np.matrix(np.zeros((int(len(mat_nodes)),1)))
+        nufission_rate = np.matrix(np.zeros((int(flux.shape[0]),1)))
+        for i in range(len(mat_nodes)):
+            nufission_xs[i,0] = material_list[mat_nodes[i]].nufission[0]
+        nufission_rate[0,0] = flux[0,0] * nufission_xs[0,0]
+        for i in range(len(mat_nodes)):
+            nufission_rate[i+1,0] = flux[i+1,0] * nufission_xs[i,0]
+        returndata = nufission_rate
+    elif mode == "leak":
+        beta_left = setting.boundary_left
+        beta_right = setting.boundary_right
+        d_xs = np.matrix(np.zeros((int(len(mat_nodes)),1)))
+        leak_rate = np.matrix(np.zeros((int(flux.shape[0]),1)))
+        for i in range(len(mat_nodes)):
+            d_xs[i,0] = material_list[mat_nodes[i]].transport[0]
+            d_xs[i,0] = 1/(3*d_xs[i,0])
+        leak_rate[0,0] = -d_xs[0,0] *(flux[1,0] - flux[0,0])/(nodes[1]-nodes[0])
+        for i in range(len(mat_nodes)):
+            leak_rate[i+1,0] =  - d_xs[i,0] * (flux[i+1,0] - flux[i,0])/(nodes[i+1] - nodes[i])
+        leak_rate[0,0],leak_rate[-1,0] = (1-beta_left)*leak_rate[0,0],(1-beta_right)*leak_rate[-1,0]
+        returndata = leak_rate
+    return returndata
+
+
 # a function for solving the martix
 def SolveMatrix(matrix_m,matrix_f,setting,material_list,coredata):
     # get the first value of k and flux ?
@@ -321,6 +486,7 @@ def SolveMatrix(matrix_m,matrix_f,setting,material_list,coredata):
     F1 = np.zeros(matrix_f.shape)
     F2 = np.zeros(matrix_f.shape)
     k_inf = [1.0]
+    k_error = []
     ord_num = 1# fanshu
     for i in range(setting.maxnoutiter):# this is the outside iteration
 
@@ -328,25 +494,48 @@ def SolveMatrix(matrix_m,matrix_f,setting,material_list,coredata):
             F1[j,0] = matrix_f[j,0] * flux[j,0]
 
         if setting.matrixsolutionmethod == "matrix inversion":
-            print(1)
             flux_i = np.linalg.inv(matrix_m)@(F1/k_inf[i])
         elif setting.matrixsolutionmethod == "thomas":
-            print(2)
+            flux_i = np.zeros(matrix_f.shape)
+            c=[]
+            u=[]
+            l=[]
+            y=[]
+            u.append(matrix_m[0,0])
+            y.append(F1[0,0]/k_inf[i])
+            for j in range(1,matrix_f.shape[0]):
+                l.append(matrix_m[j,j-1]/u[-1])
+                c.append(matrix_m[j-1,j])
+                u.append(matrix_m[j,j]-l[-1]*c[-1])
+                y.append(F1[j,0]/k_inf[i] - l[-1]*y[-1])
+            flux_i[-1,0] = y[-1]/u[-1]
+            for j in range(1,matrix_f.shape[0]):
+                flux_i[-1-j,0] = (y[-1-j]-c[-j]*flux_i[-j,0])/u[-1-j]
         else:
             ErrorFunction(15)
             os._exit(0)
 
         for j in range(matrix_f.shape[0]):
             F2[j,0] = matrix_f[j,0] * flux_i[j,0]
-
+             
         k_inf.append((k_inf[i] * np.linalg.norm(F2,ord = ord_num)/np.linalg.norm(F1,ord = ord_num)))
 
         if np.abs((k_inf[i+1] - k_inf[i])/k_inf[i + 1]) <= setting.kerrlimit:
+            k_error.append(np.abs((k_inf[i+1] - k_inf[i])/k_inf[i + 1]))
             break
         else:
             flux = flux_i
-# a function for the outputing of data
+            k_error.append(np.abs((k_inf[i+1] - k_inf[i])/k_inf[i + 1]))
+    return k_inf,k_error,flux
 # a function for the checking of other functions
-setting, material_list, coredata = InputRead("FDM_Code_input.xml")
-matrix_m,matrix_f = CreateMartix(setting,material_list,coredata)
-SolveMatrix(matrix_m,matrix_f,setting,material_list,coredata)
+# a function for main
+def main():
+    fileout = ScreenPrint("FDM_Code_input.xml")
+    setting, material_list, coredata = InputRead("FDM_Code_input.xml")
+    matrix_m,matrix_f,nodes,mat_nodes = CreateMartix(setting,material_list,coredata)
+    k_inf,k_error,flux = SolveMatrix(matrix_m,matrix_f,setting,material_list,coredata)
+    absorption_rate,nufission_rate,leak_rate = DataCalculate("absorption",material_list,setting,mat_nodes,nodes,flux),DataCalculate("nufission",material_list,setting,mat_nodes,nodes,flux),DataCalculate("leak",material_list,setting,mat_nodes,nodes,flux)
+    DataPrint(fileout,setting,k_inf,k_error,nodes,flux,absorption_rate,nufission_rate,leak_rate)
+    os.system("pause")
+
+main()
